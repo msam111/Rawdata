77ee2106cd8d2be2ee6353e5bf66387ed6468427
Henrik Bulskov <bulskov@ruc.dk>
2017-03-16 10:03:08 +0100

    created a ADO example

--- AdoExample/Program.cs
+++ AdoExample/Program.cs
@@ -1,13 +1,68 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 
 namespace AdoExample
 {
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return Name ;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Query();
 
+            Insert();
+
+        }
+
+        private static void Query()
+        {
+            using (var conn = new MySqlConnection("server=localhost;database=northwind;uid=bulskov;pwd=henrik"))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from category";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.HasRows && reader.Read())
+                {
+                    var category = new Category
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Description = reader.GetString(2)
+                    };
+
+
+                    Console.WriteLine($"Category: {category}");
+                }
+            }
+        }
+
+        private static void Insert()
+        {
+            var conn = new MySqlConnection("server=localhost;database=northwind;uid=bulskov;pwd=henrik");
+            conn.Open();
+            var cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "insert into category values (9, 'Testing', null)";
+
+            var numRows = cmd.ExecuteNonQuery();
+
+            Console.WriteLine($"Number of rows: {numRows}");
+            
         }
     }
 }
