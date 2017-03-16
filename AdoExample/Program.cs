8c99d438b9d8455eb38d432e805783efccf38039
Henrik Bulskov <bulskov@ruc.dk>
2017-03-16 09:38:10 +0100

    Add project files.

---
+++ AdoExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
