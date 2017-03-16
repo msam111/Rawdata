200bb4839c952a79351bbc1a11e6edd415bea96b
Henrik Bulskov <bulskov@ruc.dk>
2017-03-16 11:12:27 +0100

    some EF example

---
+++ EntityFrameworkExample/MyContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseMySql("server = localhost; database = northwind; uid = bulskov; pwd = henrik");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().ToTable("category");
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+        }
+    }
+}
--- EntityFrameworkExample/Program.cs
+++ EntityFrameworkExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace EntityFrameworkExample
 {
@@ -6,7 +7,30 @@ namespace EntityFrameworkExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Query();
+            Insert();
+        }
+
+        private static void Query()
+        {
+            using (var db = new MyContext())
+            {
+                var data = db.Categories.ToList();
+
+                foreach (var cat in data)
+                {
+                    Console.WriteLine(cat.Name);
+                }
+            }
+        }
+
+        private static void Insert()
+        {
+            using (var db = new MyContext())
+            {
+                db.Categories.Add(new Category { Id = 10, Name = "From EF" });
+                db.SaveChanges();
+            }
         }
     }
 }
